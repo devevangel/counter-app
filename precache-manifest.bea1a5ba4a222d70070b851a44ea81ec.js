b7aca3bde304cbb49861c1a6e15c6a5f77965c22
Evangel <40840123+mick7694@users.noreply.github.com>
2020-04-04 23:54:48 +0100

    Updates

---
+++ precache-manifest.bea1a5ba4a222d70070b851a44ea81ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afae9233a21d680aa2ca6b3b2a99a186",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "d2f72b0347a4537706f4",
+    "url": "/counter-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "828d3cab80197548d261",
+    "url": "/counter-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d2f72b0347a4537706f4",
+    "url": "/counter-app/static/js/2.231292c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-app/static/js/2.231292c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828d3cab80197548d261",
+    "url": "/counter-app/static/js/main.31f4a369.chunk.js"
+  },
+  {
+    "revision": "ff674a87fad12ba53ff1",
+    "url": "/counter-app/static/js/runtime-main.c8445eb5.js"
+  }
+]);
